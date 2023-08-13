0fb6fcf54aa4ac379abd0e4e31bef292e179056a
mltsvektr <mltsvekaterina@yandex.ru>
2023-08-13 14:36:58 +0300

    1. Убран лишний отступ внутри метода. 2. Исправлены названия переменных.

--- src/main/java/ru/maltseva/calcapp/_main/_Main.java
+++ src/main/java/ru/maltseva/calcapp/_main/_Main.java
@@ -5,12 +5,11 @@ import ru.maltseva.calcapp.util.Calculator;
 
 public class _Main {
     public static void main(String[] args) {
-        int resultOfSum = Calculator.summarize(3, 7);
-        int resultOfSubtraction = Calculator.deduct(8, 5);
-        int resultOfProduct = Calculator.multiply(5, 10);
-
-        ResultWriterService.printResult(3, 7, resultOfSum, "сложение");
-        ResultWriterService.printResult(8, 5, resultOfSubtraction, "вычитание");
-        ResultWriterService.printResult(5, 10, resultOfProduct, "умножение");
+        int sumResult = Calculator.summarize(3, 7);
+        int subtractionResult = Calculator.deduct(8, 5);
+        int productResult = Calculator.multiply(5, 10);
+        ResultWriterService.printResult(3, 7, sumResult, "сложение");
+        ResultWriterService.printResult(8, 5, subtractionResult, "вычитание");
+        ResultWriterService.printResult(5, 10, productResult, "умножение");
     }
 }
