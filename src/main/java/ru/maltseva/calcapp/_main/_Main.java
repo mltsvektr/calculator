ab873f0cc89bc42701b925991a2c3ef5b4025f60
mltsvektr <mltsvekaterina@yandex.ru>
2023-08-16 18:46:54 +0300

    1.Переиспользование переменной

--- src/main/java/ru/maltseva/calcapp/_main/_Main.java
+++ src/main/java/ru/maltseva/calcapp/_main/_Main.java
@@ -5,11 +5,11 @@ import ru.maltseva.calcapp.util.Calculator;
 
 public class _Main {
     public static void main(String[] args) {
-        int sumResult = Calculator.summarize(3, 7);
-        int subtractionResult = Calculator.deduct(8, 5);
-        int productResult = Calculator.multiply(5, 10);
-        ResultWriterService.printResult(3, 7, sumResult, "сложение");
-        ResultWriterService.printResult(8, 5, subtractionResult, "вычитание");
-        ResultWriterService.printResult(5, 10, productResult, "умножение");
+        int result = Calculator.summarize(3, 7);
+        ResultWriterService.printResult(3, 7, result, "сложение");
+        result = Calculator.deduct(8, 5);
+        ResultWriterService.printResult(8, 5, result, "вычитание");
+        result = Calculator.multiply(5, 10);
+        ResultWriterService.printResult(5, 10, result, "умножение");
     }
 }
